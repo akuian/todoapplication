ea933a4c79f9376e8f42d3523f5fedd9b0118054
akuian <hfazapratamahf@gmail.com>
2022-07-01 18:57:49 +0700

    Practicum 2

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/todos', TodoController::class);
+
 });
